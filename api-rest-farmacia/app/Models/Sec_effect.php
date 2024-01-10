52fdde3e932fdb891fd0459594d3185ece4485e6
nathaniel <natharevolution@gmail.com>
2024-01-09 23:19:47 -0400

    creando modelos y controladores

---
+++ api-rest-farmacia/app/Models/Sec_effect.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sec_effect extends Model
+{
+    use HasFactory;
+}
