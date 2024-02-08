9463c09304eb37f06f7edf8da5a9adfe4a7b3fa1
nathaniel <natharevolution@gmail.com>
2024-02-08 15:13:59 -0400

    agregando rol a apartado de perfil

--- farmacia-fullstack-fontend/src/layout/user/Profile.jsx
+++ farmacia-fullstack-fontend/src/layout/user/Profile.jsx
@@ -9,13 +9,15 @@ export const Profile = () => {
         <div className="p-2 bg-success rounded-3">
           <i class="fa-solid fa-user"></i>
         </div>
-        <h3>Nombre</h3>
+        <h3>Nombre:</h3>
         <p>{user.name}</p>
-        <h3>apellido</h3>
+        <h3>Apellido:</h3>
         <p>{user.surname}</p>
-        <h3>Correo electronico</h3>
+        <h3>Correo electronico:</h3>
         <p>{user.email}</p>
-        <h3>Creado el:</h3>
+        <h3>Rol de usuario:</h3>
+        <p>{user.role}</p>
+        <h3>Fecha de creación:</h3>
         <p>{user.created_at}</p>
       </article>
     </section>
